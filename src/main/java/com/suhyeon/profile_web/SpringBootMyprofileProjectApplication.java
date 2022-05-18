399823e2e09e813a55c2b863c332bd1ecb006797
hrno3826@naver.com <msh1126psh>
2022-05-18 15:33:10 +0900

    스프링부트 myprofile

---
+++ src/main/java/com/suhyeon/profile_web/SpringBootMyprofileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.suhyeon.profile_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyprofileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyprofileProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhyeon/profile_web/SpringBootMyprofileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhyeon.profile_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyprofileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
